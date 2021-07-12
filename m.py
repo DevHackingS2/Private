c45fbf2a0bc9a6d32f16e40343a111f63ace1e74
DevHackingS2 <87339066+DevHackingS2@users.noreply.github.com>
2021-07-12 17:49:39 -0300

    Add files via upload

---
+++ m.py
@@ -0,0 +1,269 @@
+import os
+import time
+
+os.system("clear")
+
+nome = input("Digite Seu Nome: ")
+
+os.system("clear")
+
+time.sleep(2)
+
+print ("Bem vindo {} ao TEXTINHO MEGA BLASTER ULTRA POWER RANGER REVOLUCIONÁRIO!".format(nome))
+
+time.sleep(5)
+
+os.system("clear")
+
+print ("Como não vamos poder nós vemos, eu fiz dedicamente a você!")
+
+time.sleep(5)
+
+os.system("clear")
+
+print("Espero que goste! Fiz com muito amor e paciência! Te amo, Boa Leitura! ")
+
+time.sleep(5)
+
+os.system("clear")
+
+print ("")
+
+time.sleep(10)
+
+os.system("clear")
+
+print("Há 1 ano atrás, estava muito perdido no mundo... ")
+time.sleep(7)
+
+os.system("clear")
+print("Tinha certeza que não ia durar muito com os psicológicos que estava tendo")
+
+time.sleep(7)
+
+os.system("clear")
+
+print("há 1 ano atrás, eu estava esperando o tempo de que eu não aguente e consiga a cometer meu suicídio.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Há 1 ano atrás, era só eu e meu psicológico. ")
+time.sleep(7)
+
+os.system("clear")
+
+print("Há 1 ano atrás, eu já não suportava mais acordar todos os dias e ver que ainda estava vivo.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Há 1 ano atrás, eu tive a minha salvação, que foi você.")
+time.sleep(7)
+
+os.system("clear")
+
+print("No dia 16 de julho de 2020, Eu recebo uma mensagem de uma pessoa que estava em dúvida em uma atividade no curso.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Fomos conversando sobre a atividade e meio que pedi ajuda dela porque estava interessado em outra pessoa.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Dias e dias foram se passando e vi que ela (Você) seria a pessoa ideal para mim.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Com mais ou menos 2 semanas de conversa que estavamos tendo, eu te pedi em namoro.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Foi uma atitude arriscada, porque nós se viu poucas vezes e fora que nós nunca tinha ficado.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Então seria mais um fora pra coleção, mas você disse vamos nós conhecer melhor.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Isso me deixou meio que animado. Então comecei a 'investir' em você, porque sempre quis achar uma pessoa perfeita igual você,")
+time.sleep(7)
+
+os.system("clear")
+print("Que goste das mesmas coisas que eu!")
+time.sleep(7)
+
+os.system("clear")
+print("Que bom que você mudou a minha vida.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Você me trouxe a vida novamente, você me fez como é ser feliz.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Como a ser uma pessoa amada, você me ensinou a como ter sentimentos.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Você me ensinou como é bom viver, coisa que eu não sabia. ")
+time.sleep(7)
+
+os.system("clear")
+
+print("Apenas vivia no meu mundo, não me importava com ninguém.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Mas, você apareceu na minha vida e me ensinou tudo essas coisas.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Uma das coisas que eu não quero pra nossa relação é traição e brigas.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Se não sabe o quanto eu fico mal quando nós fica brigado ou eu faço alguma coisa que você não gostou e fica chateada comigo.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Mas enfim...")
+time.sleep(7)
+
+os.system("clear")
+
+print("No dia 15 junho de 2021 tivemos nosso primeiro encontro.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Eu tava muito tímido, com vergonha por ser a primeira vez e muito feliz por isso ter acontecido.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Ainda mais com você!")
+time.sleep(7)
+
+os.system("clear")
+
+print("Mas enfim, resumindo, eu quero ter você na minha vida pra sempre")
+time.sleep(7)
+
+os.system("clear")
+
+print("Quero que você evolua juntamente a mim, quero ver você feliz por ter me escolhido diante a muitas pessoas no mundo.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Eu quero ver você seguindo oque você sempre sonhou, eu quero acordar todos os dias ao seu lado")
+time.sleep(7)
+
+os.system("clear")
+
+print("Sentir aquele bafo que mata 3 leões e 10 elefantes(Essa eu peguei pesado AKAKAKAKKAKAKAKAKKAKAK)")
+time.sleep(7)
+
+os.system("clear")
+
+print("Sentir o seu abraço gostoso.")
+time.sleep(7)
+
+os.system("clear")
+
+print("E também eu queria falar uma coisa, mas só vou falar em chamada.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Mas enfim, espero ter gostado do textinho.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Fiz porque te amo muito e sinto muita saudade de você.")
+time.sleep(7)
+
+os.system("clear")
+
+print("A saudade de você é tanta que eu não vejo a hora de te ver novamente.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Poder te abraçar novamente.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Ficar grudadinho com você")
+time.sleep(7)
+
+os.system("clear")
+
+print("Passar todo o amor que tenho pra passar.")
+time.sleep(7)
+
+os.system("clear")
+
+print("Enfim!")
+time.sleep(7)
+
+os.system("clear")
+
+print("Espero ter gostado!")
+time.sleep(7)
+
+os.system("clear")
+
+print("                     Feitinho pelo Seu Futuro Namorado!                ")
+time.sleep(10)
+
+os.system("clear")
+
+print ("""
+
+. □□　□□□
+□■■■□■■■■□
+□■■■■■■■■■□
+□■■■■■■■■□
+□■■■■■■■□
+　 □■■■■■□
+　　□■■■□
+　　　□□ 　
+。　　 /　　☆+
+　☆。/
+　　 /　
+　　/
+　 (　　＋☆
+　　＼　　　　。
+　　　＼
+　　　 )
+　　　/
+。　 / I しoνe..
+　　/　　 yoυ♡
+　　 ♡
+　 ＼●/
+　　 ■
+　  ||
+
+""")
+
+time.sleep(10)
+
+os.system("clear")
